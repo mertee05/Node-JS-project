122f2c7d33160d0350c7fd07c751d5b9e57ef9db
mertee05 <131835458+mertee05@users.noreply.github.com>
2024-04-09 11:37:08 -0700

    Finishing up
    Finishing up template literals and clearing comments

--- postMyCity.js
+++ postMyCity.js
@@ -32,9 +32,6 @@ app.post("/city", (req, res) => {
       console.log(
         `Current weather in ${req.body.CityName}:\n\n - Temperature: ${weatherData.current.temp_f}°F  `
       );
-      // res.send(
-      //   `{ "WeatherInfo" : "Current weather in ${req.body.CityName}: - Temperature: ${weatherData.current.temp_f}°F"  }`
-      // );
       res.json(weatherData);
     })
     .catch((error) => {
@@ -45,61 +42,3 @@ app.post("/city", (req, res) => {
     });
   console.log("Post Request");
 });
-
-// app.get("/city", (req, res) => {
-//   res.json(globalData.cityData);
-//   console.log(globalData.cityData, "Get Request");
-// });
-// var CITY = cityData.CityName.value;
-// var apiURL2 = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${response.CityNameBE}`;
-
-// app.get("/", (req,res)=> {
-//   axios
-// })
-
-// axios
-//   .get(apiURL2)
-//   .then((response) => {
-//     const weatherData = response.data;
-//     console.log(
-//       `Current weather in ${response.city_name}:\n\n - Temperature: ${weatherData.current.temp_f}°F  `
-//     );
-//     res.send(
-//       `Current weather in ${response.city_name}:\n\n - Temperature: ${weatherData.current.temp_f}°F  `
-//     );
-//   })
-//   .catch((error) => {
-//     console.error(
-//       "Eror fetching weather data:",
-//       error.response.data.error.message
-//     );
-//   });
-
-// app.post("/City", function (req, res) {
-//   console.log(req.body);
-// response = {
-//   city_name: req.body.city_name,
-// };
-
-// console.log(response);
-
-// var apiURL2 = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${response.city_name}`;
-
-// axios
-//   .get(apiURL2)
-//   .then((response) => {
-//     const weatherData = response.data;
-//     console.log(
-//       `Current weather in ${response.city_name}:\n\n - Temperature: ${weatherData.current.temp_f}°F  `
-//     );
-//     res.send(
-//       `Current weather in ${response.city_name}:\n\n - Temperature: ${weatherData.current.temp_f}°F  `
-//     );
-//   })
-//   .catch((error) => {
-//     console.error(
-//       "Eror fetching weather data:",
-//       error.response.data.error.message
-//     );
-//   });
-// });
